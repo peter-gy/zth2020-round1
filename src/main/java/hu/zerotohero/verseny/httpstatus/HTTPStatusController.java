b1574296814784a9c324373d187e7d231ed2e382
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-07-21 15:08:42 +0200

    HTTPStatusController class implemented

--- src/main/java/hu/zerotohero/verseny/httpstatus/HTTPStatusController.java
+++ src/main/java/hu/zerotohero/verseny/httpstatus/HTTPStatusController.java
@@ -1,9 +1,11 @@
 package hu.zerotohero.verseny.httpstatus;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("http-status")
@@ -11,6 +13,11 @@ public class HTTPStatusController {
 
     @GetMapping("/getStatusDescription")
     public String getStatusDescription(@RequestParam(value = "statusCode", defaultValue = "500") Integer statusCode) {
-        return "Hi"; //TODO implement me!
+        try {
+            return HttpStatus.valueOf(statusCode).name();
+        } catch (IllegalArgumentException e) {
+            String reason = String.format("[%d] is not a valid status code.", statusCode);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason);
+        }
     }
 }
